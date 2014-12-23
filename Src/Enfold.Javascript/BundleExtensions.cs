85824fec76df057287c049c8d04d171f05b218f5
achingono <achingono@gmail.com>
2014-12-23 14:30:35 -0500

    Clean up

--- Src/Enfold.Javascript/BundleExtensions.cs
+++ Src/Enfold.Javascript/BundleExtensions.cs
@@ -1,11 +1,8 @@
 ﻿using Enfold.Configuration;
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Web;
 using System.Web.Hosting;
 using System.Web.Optimization;
