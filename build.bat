7bd8e3c2c9b86962a942936fbe752efe7e63ecea
Alfero Chingono <achingono@gmail.com>
2016-01-19 17:52:53 -0500

    Update build.bat
    RE: https://docs.nuget.org/consume/package-restore

--- build.bat
+++ build.bat
@@ -14,6 +14,8 @@ if "%nuget%" == "" (
 	set nuget=nuget
 )
 
+%nuget% restore Src
+
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\Enfold.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
 mkdir Build
